3da7a6b1227f7d45243079a79a8dc070d76b9bcf
harshvardhan9661 <harshvardhan9661@gmail.com>
2023-08-10 14:13:34 +0530

    harshvardhan cooment 1

--- src/main/java/pages/Loginpage.java
+++ src/main/java/pages/Loginpage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import utility.Elementutility;
-
+// comment here 
 public class Loginpage {
     WebDriver driver;
     Elementutility elementutil;
